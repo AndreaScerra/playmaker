df0dd1a5f9a639c918359943118d7dfb57b54aef
Andrea Scerra <108824127+AndreaScerra@users.noreply.github.com>
2024-11-13 15:18:43 +0100

    Add files via upload

---
+++ home.js
@@ -0,0 +1,8 @@
+document.getElementById('go-playmaker').addEventListener('click', () => {
+    window.location.href = './playmaker/index.html';
+});
+
+document.getElementById('go-pdf').addEventListener('click', () => {
+    window.location.href = './png-to-pdf/ptp.html';
+});
+
