082e249155ff51902e675fbca736fbcfd7eb1af8
Andrea Scerra <108824127+AndreaScerra@users.noreply.github.com>
2025-04-18 13:46:26 +0200

    Update home.js

--- home.js
+++ home.js
@@ -1,8 +1,9 @@
 document.getElementById('go-playmaker').addEventListener('click', () => {
-    window.location.href = './tabopc.html';
+    window.location.href = 'tabopc.html';
 });
 
 document.getElementById('go-pdf').addEventListener('click', () => {
-    window.location.href = './png-to-pdf/ptp.html';
+    window.location.href = '/png-to-pdf/ptp.html';
 });
+
 
